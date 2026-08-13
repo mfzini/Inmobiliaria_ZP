81406b360870f55c67c1bbd8827c35989dc8c423
Alejandro Peralta <158860526+AleS485@users.noreply.github.com>
2026-08-13 16:54:50 -0300

    feat: repositoriobase

---
+++ Repositories/RepositorioBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace inmobiliaria.Repositories
+{
+	public abstract class RepositorioBase
+	{
+		protected readonly IConfiguration configuration;
+		protected readonly string connectionString;
+
+		protected RepositorioBase(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+			connectionString = configuration["ConnectionStrings:DefaultConnection"];
+			//connectionString = configuration["ConnectionStrings:MySql"];
+		}
+	}
+}
