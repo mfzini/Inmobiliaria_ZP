6b93f07f7eebcbbcb215860a8915d23b09540059
Alejandro Peralta <158860526+AleS485@users.noreply.github.com>
2026-08-30 19:29:40 -0300

    feat: agregado editar en controllerInmueble

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -54,9 +54,70 @@ public class InmuebleController(InmuebleRepository inmuebleRepo, PersonaReposito
     }
 
     [HttpGet]
-    public IActionResult Editar()
+    public IActionResult Editar(string id)
     {
-        return View();
+        if(string.IsNullOrEmpty(id))
+        {
+            return RedirectToAction(nameof(Listar));
+        }
+        try
+        {
+            var inmueble = inmuebleRepo.GetById(id);
+            if(inmueble == null)
+            {
+                Response.StatusCode = 404;
+                //todo.
+            }
+
+            var dto = new InmuebleDTO
+            {
+                Propietario = inmueble.Propietario.Dni,
+                Tipo = inmueble.Tipo.Id,
+                Direccion = inmueble.Direccion,
+                Capacidad = inmueble.Capacidad,
+                Precio = inmueble.Precio,
+                Listado = inmueble.Listado,
+                Latitud = inmueble.Latitud,
+                Longitud = inmueble.Longitud
+            };
+
+            ViewBag.InmuebleId = id;
+            return View(dto);
+
+        }catch(Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            return RedirectToAction(nameof(Listar));
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Editar(string id, InmuebleDTO dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.InmuebleId = id;
+            return View(dto);
+        }
+
+        Persona? propietario = personaRepo.FindByDni(dto.Propietario);
+        TipoInmueble? tipo = inmuebleRepo.FindTipoByID(dto.Tipo);
+
+        Inmueble inmueble = new Inmueble
+        {
+            Id = id,
+            Propietario = propietario,
+            Tipo = tipo,
+            Direccion = dto.Direccion,
+            Capacidad = dto.Capacidad,
+            Precio = dto.Precio,
+            Listado = dto.Listado,
+            Latitud = dto.Latitud,
+            Longitud = dto.Longitud
+        };
+
+        inmuebleRepo.Update(inmueble);
+        return RedirectToAction(nameof(Listar));
     }
 
     [HttpGet]
