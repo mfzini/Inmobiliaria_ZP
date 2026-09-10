8d5d8e507528fd4b1ae110aa38ab0962cd5a33e4
Cisco <mfzini.dev@gmail.com>
2026-09-10 20:16:39 +0000

    me olvide los joins xd

--- Repositories/InmuebleRepository.cs
+++ Repositories/InmuebleRepository.cs
@@ -224,15 +224,15 @@ public class InmuebleRepository(IConfiguration configuration) : RepositorioBase(
     public List<Inmueble> ListConMasReservas365Dias()
     {
         List<Inmueble> inmuebles = [];
-        var query = @"select *, count(r.id) as total_reservas, r.id as r_id, i.id as i_id, p.nombre as p_nombre, t.id as t_id, t.nombre as t_nombre
-        from Reservas r
-        join Inmuebles i on i.id = r.inmueble
-        join TipoInmueble t on t.id = i.tipo
-        join Personas p on p.dni = i.propietario
-        where datediff(now(), r.fecha_inicio) < 365
-        group by r.inmueble
-        order by total_reservas desc
-        limit 5;";
+            var query = @"select *, count(r.id) as total_reservas, r.id as r_id, i.id as i_id, p.nombre as p_nombre, t.id as t_id, t.nombre as t_nombre
+            from Reservas r
+            join Inmuebles i on i.id = r.inmueble
+            join TipoInmueble t on t.id = i.tipo
+            join Personas p on p.dni = i.propietario
+            where datediff(now(), r.fecha_inicio) < 365
+            group by r.inmueble
+            order by total_reservas desc
+            limit 5;";
         using MySqlConnection connection = new(connectionString);
         using MySqlCommand command = new(query, connection);
         connection.Open();
@@ -268,13 +268,16 @@ public class InmuebleRepository(IConfiguration configuration) : RepositorioBase(
     {
         List<Inmueble> inmuebles = [];
         var query = @"select *, r.id as r_id, i.id as i_id, p.nombre as p_nombre, t.id as t_id, t.nombre as t_nombre
-        where i.listado = 1
-        and not exists (
-            select 1 from reservas r
-            where i.id = r.inmueble
-            and r.fecha_inicio <= @hasta
-            and r.fecha_fin >= @desde
-        )";
+            from Inmuebles i
+            join Personas p on p.dni = i.propietario
+            join TipoInmueble t on t.id = i.tipo
+            where i.listado = 1
+            and not exists (
+                select 1 from reservas r
+                where i.id = r.inmueble
+                and r.fecha_inicio <= @hasta
+                and r.fecha_fin >= @desde
+            )";
 
         using MySqlConnection connection = new(connectionString);
         using MySqlCommand command = new(query, connection);
@@ -292,7 +295,8 @@ public class InmuebleRepository(IConfiguration configuration) : RepositorioBase(
     public List<Inmueble> ListarByDireccion(string direccion)
     {
         List<Inmueble> inmuebles = [];
-        var query = @"select *, i.id as i_id, p.nombre as p_nombre, t.id as t_id, t.nombre as t_nombre from Inmuebles i
+        var query = @"select *, i.id as i_id, p.nombre as p_nombre, t.id as t_id, t.nombre as t_nombre
+            from Inmuebles i
             join Personas p on i.propietario = p.dni
             join TipoInmueble t on i.tipo = t.id
             where i.direccion like @direccion";
